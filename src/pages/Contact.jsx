1978136ca51cebb704aa890485f85928bc1d0cf9
Prosper-ekube <prosperekube@gmail.com>
2025-06-29 20:02:55 +0100

    feat: add intro section to Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
-};
+  return <div>
+    <div className="p-[20px]">
+      <h1 className="font-bold font-serif text-green-900 text-center mb-[10px] lg:mb-[15px] text-2xl md:text-3xl lg:text-4xl/12 xl:text-5xl/14 2xl:text-8xl/28">
+        Contact the Faculty of<br /> Vocational & Technical Education</h1>
+      <h2 className="mx-auto font-normal font-sans text-center text-base md:text-xl lg:text-3xl/10 xl:text-4xl/12 2xl:text-6xl/24 
+      lg:w-[1000px] xl:w-[1200px] 2xl:w-[83.33vw]">We are here to assist current and prospective students,
+        partners and the community. Reach out to us - We look forward to connecting!</h2>
+    </div>
+  </div>
+}
 
 export default Contact;
 
